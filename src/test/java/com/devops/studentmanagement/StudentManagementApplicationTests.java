062ab175ee3fdd547e49268e1d847568cb181662
Anji-44 <anjiprabha007@gmail.com>
2026-08-20 15:36:17 +0000

    Updated Application to Version 21

--- src/test/java/com/devops/studentmanagement/StudentManagementApplicationTests.java
+++ src/test/java/com/devops/studentmanagement/StudentManagementApplicationTests.java
@@ -21,6 +21,6 @@ class StudentManagementApplicationTests {
     void studentsApiShouldReturnSuccess() throws Exception {
         mockMvc.perform(get("/students"))
                 .andExpect(status().isOk())
-                .andExpect(content().string("Student Management System - Automatic Deployment Version 20 Successfully!"));
+                .andExpect(content().string("Student Management System - Automatic Deployment Version 21 Successfully!"));
     }
 }
